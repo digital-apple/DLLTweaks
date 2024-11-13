98f42f75c89c0704427904fe300a52593f66fd66
digital-apple <115373190+digital-apple@users.noreply.github.com>
2024-11-13 10:01:16 -0300

    1.2.0
    - Fixed Tweaks::ConcentrationCasting for Skyrim SE version 1.6+
    
    - Some minor refactoring and commenting

--- source/Hooks.cpp
+++ source/Hooks.cpp
@@ -22,7 +22,7 @@ namespace Fixes
 
     struct ScaleMovementSpeed
     {
-        static float Call(RE::TESObjectREFR* a_reference)
+        static auto Call(RE::TESObjectREFR* a_reference) -> float
         {
             const auto scale = Callback(a_reference);
 
@@ -61,52 +61,53 @@ namespace Tweaks
         }
     };
 
-    // Concentration spells that costs more (per second) than the current magicka amount can no longer be casted.
+    // Concentration spells that costs more (per second) than the actor's total magicka amount can no longer be casted.
 
     struct ConcentrationCasting
     {
-        static auto Call(RE::Actor* a_actor, RE::ActorValue a_value, RE::MagicItem* a_spell, float a_cost, bool a_useBaseValueForCost) -> bool
+        static bool Call(RE::Actor* a_actor, RE::ActorValue a_value, RE::MagicItem* a_spell, float a_cost, bool a_useBaseValueForCost)
         {
-            INFO("Actor: <{}> | Actor Value: <{}> | Spell: <{}> | Cost: <{}> | Use Base Value: <{}>", a_actor->GetName(), std::to_string(a_value).c_str(), a_spell->GetName(), a_cost, a_useBaseValueForCost);
-
             const auto result = a_useBaseValueForCost ? a_actor->AsActorValueOwner()->GetBaseActorValue(a_value) : a_actor->AsActorValueOwner()->GetActorValue(a_value);
 
             if (a_spell->GetCastingType() == RE::MagicSystem::CastingType::kConcentration) {
                 const auto value_maximum = a_actor->AsActorValueOwner()->GetPermanentActorValue(a_value);
 
-                INFO("Value Maximum: <{}>", value_maximum);
-
                 return value_maximum >= a_cost;
             }
-
-            INFO("Result: <{}>", result);
 
             return result >= a_cost;
         }
     };
+
+    // Decreases jump height while sneaking.
 
     struct PlayerUpdate
     {
         static void Call(RE::PlayerCharacter* player, float delta) 
         {
             if (player->IsSneaking()) {
-                jumpHeightPlayer(player, Settings::SneakJumpHeightMod);
+                JumpHeightPlayer(player, Settings::SneakJumpHeightMod);
             }
+
             return func(player, delta);
         }
-        static bool jumpHeightPlayer(RE::PlayerCharacter* player, float height_mod) 
+
+        static bool JumpHeightPlayer(RE::PlayerCharacter* player, float height_mod) 
         {
             if (!player->IsInMidair()) {
                 player->GetCharController()->jumpHeight *= height_mod;
                 player->GetCharController()->fallStartHeight *= height_mod;
                 player->GetCharController()->fallTime *= height_mod;
+
                 return true;
             }
+
             return false;
         }
-        static void InstallPlayerUpdate() {
+        static void InstallPlayerUpdate()
+        {
             REL::Relocation<std::uintptr_t> player_vtbl{RE::VTABLE_PlayerCharacter[0]};
-            func = player_vtbl.write_vfunc(0xad, Call);
+            func = player_vtbl.write_vfunc(0xAD, Call);
         }
         inline static REL::Relocation<decltype(Call)> func;
     };
@@ -118,15 +119,20 @@ namespace Tweaks
             stl::write_call<AbsorptionChance>(target.address());
 
             // Clean up garbage instructions.
-
-            std::uint8_t NOP[] = { 0x90, 0x90, 0x90 };
-            REL::safe_write(target.address() + 0x5, NOP, 3);
+            stl::safe_write(target.address() + 0x5, std::array<std::uint8_t, 3>{ 0x90, 0x90, 0x90 });
 
             INFO("Tweaks @ Installed <{}>", typeid(AbsorptionChance).name());
         }
 
-        if (Settings::ConcentrationCasting && REL::Module::GetRuntime() == REL::Module::Runtime::SE) {
-            REL::Relocation target{ RELOCATION_ID(33364, 34145), REL::Relocate(0x1B4, 0x1b6) };
+        if (Settings::ConcentrationCasting) {
+            REL::Relocation target{ RELOCATION_ID(33364, 34145), REL::Relocate(0x1B4, 0x1B3) };
+
+            /// CALL    [RAX+0x2A8]
+            if (!REL::make_pattern<"FF 90 A8 02 00 00">().match(target.address() + 0x1E)) {
+                ERROR("Tweaks @ Failed to match byte pattern for <{}>", typeid(ConcentrationCasting).name());
+
+                return;
+            }
 
             ///// MOV    RCX, [RBX+0xB8] == Actor*
             stl::safe_write(target.address() + 0x00, std::array<std::uint8_t, 7>{ 0x48, 0x8B, 0x8B, 0xB8, 0x00, 0x00, 0x00 });
@@ -146,16 +152,19 @@ namespace Tweaks
             ///// MOV    [RSP+0x20], AL == Use Base Cost
             stl::safe_write(target.address() + 0x16, std::array<std::uint8_t, 4>{ 0x88, 0x44, 0x24, 0x20 });
 
+            // (void*, int, void*, float, float)
             stl::write_call<ConcentrationCasting>(target.address() + 0x1A);
 
+            // Clean up garbage instructions.
             stl::safe_write(target.address() + 0x1F, std::array<std::uint8_t, 25>{ 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 });
 
             INFO("Tweaks @ Installed <{}>", typeid(ConcentrationCasting).name());
         }
 
-        if (Settings::SneakJumpHeightEnable) {
+        if (Settings::SneakJumpHeight) {
             PlayerUpdate::InstallPlayerUpdate();
-            INFO("Tweak @ Installed <{}>", typeid(PlayerUpdate).name());
+
+            INFO("Tweaks @ Installed <{}>", typeid(PlayerUpdate).name());
         }
     }
 }
